6d0cda38aa27fc890f4d08ebf21cab71bca1d8c2
Thomas Roos <throos@amazon.de>
2023-12-11 15:09:36 +0000

    fix multicast receiver

--- meta-aws-iot-automotive-demo/recipes-connectivity/vcan-ec2-multicast-receiver/files/vcan-ec2-multicast-receiver.sh
+++ meta-aws-iot-automotive-demo/recipes-connectivity/vcan-ec2-multicast-receiver/files/vcan-ec2-multicast-receiver.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+ip link set up vcan0
+
 export IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 echo "my ip: $IP"
 socat UDP4-RECVFROM:3030,ip-add-membership=239.255.0.1:$IP,fork - | while read line; do cansend vcan0 $line; done
