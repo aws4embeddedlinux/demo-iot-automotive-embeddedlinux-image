f2a048bf4cfdbe066918ce91618942926880c170
Abdelhalim Dadouche <adadouch@amazon.fr>
2025-03-28 17:41:05 +0100

    Major code refactoring: - used a construct to handle the AWS IoT Greengrass fleet provisioning requirement and the certificate creation - provided example for all 3 scenarios (ec2, qemu and gold box)

---
+++ cdk/lib/biga-iot-resources.ts
@@ -0,0 +1,53 @@
+import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import {
+    CfnThingGroup,
+} from 'aws-cdk-lib/aws-iot';
+
+import { GreengrassFleetProvisioning } from 'cdk-iot-greengrass-fleet-provisioning';
+
+export interface BigaIoTResourcesProps extends cdk.StackProps {
+    resource_prefix: string,
+    certificateBucket: s3.Bucket,
+    certificatePrefix: string;
+    thingGroupName: string;
+}
+
+export class BigaIoTResourcesStack extends cdk.Stack {
+    public readonly certificateId: string;
+    public readonly certificateArn: string;
+    public readonly certificateBucket: s3.Bucket;
+    public readonly certificatePrefix: string;
+    public readonly thingGroup: cdk.aws_iot.CfnThingGroup;
+    public readonly region: string;
+    public readonly roleAlias: string;
+    public readonly iotDataEndpoint: string;
+    public readonly iotCredentialEndpoint: string;
+
+    constructor(scope: Construct, id: string, props: BigaIoTResourcesProps) {
+        super(scope, id, props);
+
+        this.certificateBucket = props.certificateBucket;
+        this.certificatePrefix = props.certificatePrefix;
+
+        this.thingGroup = new CfnThingGroup(this, `${props.resource_prefix}-thing-group`, {
+            thingGroupName: props.thingGroupName,
+        });
+
+        const cn: GreengrassFleetProvisioning = new GreengrassFleetProvisioning(this, `${props.resource_prefix}-greengrass-fleet-provisioning`, {
+            env: props.env!,
+            resourcePrefix: props.resource_prefix,
+            certificateBucket: this.certificateBucket,
+            certificatePrefix: this.certificatePrefix,
+        });
+
+        this.certificateId = cn.getFleetProvisioningCertificateId();
+        this.certificateArn = cn.getFleetProvisioningCertificateArn();
+
+        this.region = props.env!.region!;
+        this.roleAlias = cn.getTokenExchangeRoleAlias().roleAlias!;
+        this.iotDataEndpoint = cn.getIoTDataEndpoint();
+        this.iotCredentialEndpoint = cn.getIoTCredentialEndpoint();
+    }
+}
---
+++ source-repo-biga/embedded-linux-code-pipeline/cleanup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rm -rf .git
+rm -rf .repo
+rm -rf bsp
+rm -rf external
+rm -rf meta-agl-*
+
